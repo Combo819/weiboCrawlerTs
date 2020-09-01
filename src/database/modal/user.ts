edd2ee7b0eda61ec9178d974c9335acb73889616
combo <617512696@qq.com>
2020-08-31 21:11:39 -0400

    add Iuser interface

--- src/database/modal/user.ts
+++ src/database/modal/user.ts
@@ -1,5 +1,19 @@
 import { Document, Model, model, Types, Schema, Query } from "mongoose";
 
+/**
+ * Interface to model the User Schema for TypeScript.
+ */
+export interface IUser extends Document {
+  id:number,
+  screenName:string,
+  profileUrl:string,
+  gender:string,
+  followersCount: number,
+  followCount: number,
+  profileImageUrl: number, // the smaller profile image
+  avatarHd: string, // a larger profile image,
+}
+
 export const userSchema = new Schema({
   id: { type: Number, unique: true, required: true },
   screenName: {type:String,unique:true,required:true}, // the name shown on weibo
@@ -12,4 +26,6 @@ export const userSchema = new Schema({
   avatarHd: String, // a larger profile image,
 });
 
-export default model("User", userSchema);
+const User: Model<IUser> = model("User", userSchema);
+
+export default User;
