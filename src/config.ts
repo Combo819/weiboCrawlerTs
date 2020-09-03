9f1cf9c5b469b9bb558d06c85de22ce7f365e6e2
combo <617512696@qq.com>
2020-09-03 17:16:37 -0400

    test and work

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ const rawData:string = fs.readFileSync(path.resolve(__dirname, 'credential.json'
 const URI: string = "mongodb://localhost:27017/weiboCrawler"; //mongoDB uri
 const BASE_URL: string = "https://m.weibo.cn";
 const Q_CONCURRENCY: number = 1;
-const WEIBO_ID = "4545111997810492";
+const WEIBO_ID = "4136385227662707";
 const token: string = JSON.parse(rawData).token ;
 if(!token){
   throw new Error(`The token doesn't exist. Add a token to the src/credential`)
